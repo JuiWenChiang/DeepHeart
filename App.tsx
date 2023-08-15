ea6a9d2d4a4e5d61621f5ef856dd785dac8feae3
juiwen <juiwenchiang1995@gmail.com>
2023-08-15 20:52:12 +0800

    init: env

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import HomePage from './pages/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>DeepHeart</Text>
     </View>
   );
 }
@@ -16,5 +16,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 25, // 设置字体大小为 24
+    fontWeight: 'bold',
+    color: 'blue',
   },
 });
